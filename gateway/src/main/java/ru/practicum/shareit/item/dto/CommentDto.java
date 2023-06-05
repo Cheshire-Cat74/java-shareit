05a45afc72a82dd2e74f60b2bc9d941dc19cae3a
Viktoria <vicka.cheshirecat@gmail.com>
2023-06-06 00:30:10 +0600

    Спринт 16

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import javax.validation.constraints.NotBlank;
-
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
